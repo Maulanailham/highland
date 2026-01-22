b362c19354a1d28228c5126e2d3bf04bd98f548f
maulanailham <maulanailham1995@gmail.com>
2026-01-23 05:58:55 +0700

    attempt to amend middleware

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,11 @@ import { authConfig } from "./auth.config";
 
 export const { auth: middleware } = NextAuth(authConfig);
 
+export default middleware((req) => {
+  // Kamu bisa membiarkan kosong jika hanya ingin menjalankan
+  // logika 'authorized' yang ada di auth.config.ts
+});
+
 //configure which paths the middleware should run on
 
 export const config = {
